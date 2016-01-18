908dc86c583691b2269c25b273a91b769331327b
kitkat321 <ankit.deogharia@gmail.com>
2016-01-18 15:25:42 -0500

    Cleaned up unused variables on RobotMap

--- src/org/usfirst/frc/team4611/robot/RobotMap.java
+++ src/org/usfirst/frc/team4611/robot/RobotMap.java
@@ -15,18 +15,6 @@ public class RobotMap {
     // Joystick  ports
     public static final int leftJS = 1;
     public static final int rightJS = 2;
-
-    // button board
-    public static final int BB = 3;
-
-    // L.A  values
-    public static final int ACTUATOR_PORT = 6;
-    public static final int TOP_LIMIT_PORT = 0;
-    public static final int BOTTOM_LIMIT_PORT = 1;
-    public static final int WINCH_LIMIT_PORT_TOP = 2;
-    public static final int WINCH_LIMIT_PORT_BOTTOM = 3;
-    public static final int ClawLimitPort = 4;
-
     public static final int LAcSpeed = 1;
 
     public static final int solePort1 = 0;
@@ -34,18 +22,8 @@ public class RobotMap {
 
     public static final double soleTime = 5.0;
 
-    public static final int ascendant = 4;
-    public static final int winchMotor = 1;
-
-    public static final double forwardWinchSpeed = .75;
-    public static final double backwardWinchSpeed = -.45;
-
     public static final int leftWheelShooter = 5;
     public static final int rightWheelShooter = 6;
     public static final double wheelForwardSpeed = 1.0;
     public static final double wheelReverseSpeed = -0.5;
-
-    public static boolean DirFaced = false;
-
-    public static double filterGrain = .5;
 }
