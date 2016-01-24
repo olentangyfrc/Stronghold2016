9e6a092d9fe16ff6fa8fc06f6b180fb1cb45440c
kitkat321 <ankit.deogharia@gmail.com>
2016-01-24 17:31:00 -0500

    Added experimental code for file output of rangefinder values

--- src/org/usfirst/frc/team4611/robot/commands/leftTank.java
+++ src/org/usfirst/frc/team4611/robot/commands/leftTank.java
@@ -1,7 +1,12 @@
 package org.usfirst.frc.team4611.robot.commands;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.usfirst.frc.team4611.robot.Robot;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -11,11 +16,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class leftTank extends Command {
 
     double joyVal;
+    public Timer timer;
+    public double initialTime;
+    FileOutputStream out;
 
     public leftTank() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         this.requires(Robot.leftS);
+        this.initialTime = this.timer.getFPGATimestamp();
     }
 
     // Called just before this Command runs the first time
@@ -38,9 +47,20 @@ public class leftTank extends Command {
         this.joyVal = Robot.oi.filter(Robot.oi.leftJoy.getY());
         Robot.leftS.move(this.joyVal);
         double value = Robot.oi.ai.getAverageValue();
-        double distance = (value * 0.49)/100;
+        double distance = (value * 0.49) / 100;
         SmartDashboard.putNumber("Range Finder Average Voltage", value);
         SmartDashboard.putNumber("Calculated Distance", distance);
+        File data = new File(
+                "C:/Users/Ankit Deogharia/workspace/Stronghold2016/src/DataFile.txt");
+        double elapsedTime = this.timer.getFPGATimestamp() - this.initialTime;
+        try {
+            this.out = new FileOutputStream(data);
+            String content = "" + elapsedTime + "," + distance + "\n";
+            byte[] values = content.getBytes();
+            this.out.write(values);
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -58,5 +78,6 @@ public class leftTank extends Command {
     // subsystems is scheduled to run
     @Override
     protected void interrupted() {
+        this.out.close();
     }
 }
