7dc00bd0e366352187e8873ab999220cd25bdc94
Ankit Deogharia <ankit.deogharia@gmail.com>
2016-02-12 10:28:17 -0500

    Made small changes to the PIDSubsystem to utitize the calculated values from the PIDController

--- src/org/usfirst/frc/team4611/robot/subsystems/GrabberClaw.java
+++ src/org/usfirst/frc/team4611/robot/subsystems/GrabberClaw.java
@@ -4,44 +4,42 @@ import org.usfirst.frc.team4611.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
-import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
-import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 
 /**
  *
  */
 public class GrabberClaw extends PIDSubsystem {
-	public static Potentiometer pot;
-	public static Victor clawMotor; //not sure if this is the right class, if it doesn't work try again with regular victor
+    public static Potentiometer pot;
+    public static Victor clawMotor; //not sure if this is the right class, if it doesn't work try again with regular victor
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-    public GrabberClaw(/*double p, double i, double d*/) {
-    	super ("Claw", 2.0, 0.0, 0.0);
-    	AnalogInput ai = new AnalogInput(1);
-    	pot = new AnalogPotentiometer(ai, .1, 0);
-		clawMotor = new Victor(RobotMap.clawMotorPort);
-		// TODO Auto-generated constructor stub
-	}
+    public GrabberClaw(/* double p, double i, double d */) {
+        super("Claw", 2.0, 0.0, 0.0);
+        AnalogInput ai = new AnalogInput(1);
+        pot = new AnalogPotentiometer(ai, .1, 0);
+        clawMotor = new Victor(RobotMap.clawMotorPort);
+        this.getPIDController().setPercentTolerance(15);
+    }
 
-	public void initDefaultCommand() {
+    @Override
+    public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
 
-	@Override
-	public double returnPIDInput() {
-		//TODO Auto-generated method stub
-		return ((AnalogInput) pot).getAverageVoltage();
-	}
+    @Override
+    public double returnPIDInput() {
+        //TODO Auto-generated method stub
+        return ((AnalogInput) pot).getAverageVoltage();
+    }
 
-	@Override
-	public void usePIDOutput(double output) {
-		// TODO Auto-generated method stub
-		clawMotor.set(output);
-	}
+    @Override
+    public void usePIDOutput(double output) {
+        // TODO Auto-generated method stub
+        clawMotor.pidWrite(output);
+    }
 }
-
