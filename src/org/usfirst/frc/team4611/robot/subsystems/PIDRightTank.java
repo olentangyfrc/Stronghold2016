648f90f0ff9be587d2c5541267895f6ea84e24e4
Divyaditya Shrivastava <shrivastavad24@gmail.com>
2016-02-14 15:28:55 -0500

    added PID Tanks for vision

---
+++ src/org/usfirst/frc/team4611/robot/subsystems/PIDRightTank.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team4611.robot.subsystems;
+
+import org.usfirst.frc.team4611.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+/**
+ *
+ */
+public class PIDRightTank extends PIDSubsystem {
+	private Victor frontR;
+	private Victor backR;
+    // Initialize your subsystem here
+    public PIDRightTank(double p, double i, double d, double f) {
+    	super("PID Right Tank", p, i, d, f);
+        // Use these to get going:
+        // setSetpoint() -  Sets where the PID controller should move the system
+        //                  to
+        // enable() - Enables the PID controller.
+    	setSetpoint(RobotMap.centerXOfficial);
+    	enable();
+    }
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    protected double returnPIDInput() {
+        // Return your input value for the PID loop
+        // e.g. a sensor, like a potentiometer:
+        // yourPot.getAverageVoltage() / kYourMaxVoltage;
+    	return 0.0;
+    }
+    
+    protected void usePIDOutput(double output) {
+        // Use output to drive your system, like a motor
+        // e.g. yourMotor.set(output);
+    }
+}
